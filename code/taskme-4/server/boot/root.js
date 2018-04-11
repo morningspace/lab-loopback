aa2af09e5480ff8249ddf5108cca6be63024bd88
morningspace <morningspace@yahoo.com>
2018-04-12 07:17:12 +0800

    revert stuff for episode IV

--- code/taskme-4/server/boot/root.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-  // Install a `/` route that returns server status
-  var router = app.loopback.Router();
-  router.get('/', app.loopback.status());
-  app.use(router);
-};
